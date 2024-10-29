4e0f5b86b87cda9dad6df78f36e995c51a96093f
Kirsten <Kigaa19@student.sdu.dk>
2024-10-29 13:49:52 +0100

    ClassPlayer_MetodeLoseHealth+RestoreHealth

---
+++ Player.cs
@@ -0,0 +1,17 @@
+public class Player
+{
+ // atribut
+ public int Health;  
+ 
+ //metode
+ public void LoseHealth()
+ {
+     Health = Health--;
+     return (void);
+}
+
+ public void ReStoreHealth()
+ {
+     Health = Health++;
+     return (void);
+ }
