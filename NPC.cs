e78a24737198a830a79548fa65eb1373b0cc9c93
Rothausen <rasmusrothausen@gmail.com>
2024-10-29 20:26:26 +0100

    Start conversation

--- NPC.cs
+++ NPC.cs
@@ -17,6 +17,30 @@ public class NPC
         else 
             ConsoleWriteLine("This person does not show any warningsigsn")        
     }
-    public void StartConversation
+    public void StartConversation()
+    {
+        Console.WriteLine("Starting conversation");
+        Console.WriteLine("1. Greetings friend, How are you doing today?");
+        Console.WriteLine("2. Sup nerd hows it hangin?");
+        Console.WriteLine("3. Nevermind, i got somewhere else to be!");
+
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                Console.WriteLine($"I am doign very well thank you, What are you doing today?.");
+                break;
+            case "2":
+                Console.WriteLine("Im Skibidi Rizz Fam, Whats the sitch?");
+                break;
+            case "3":
+                Console.WriteLine("Allrighty then, have a good day then");
+                break;
+            default:
+                Console.WriteLine("I don't understand that....");
+                break;
+        }
+    }
     
 }
