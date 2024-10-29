61648c404207a4b051a6eef8f34eb43082400bf9
Rothausen <rasmusrothausen@gmail.com>
2024-10-29 14:30:14 +0100

    NPC
    Vere good npc kode

--- NPC.cs
+++ NPC.cs
@@ -2,5 +2,21 @@ namespace DefaultNamespace;
 
 public class NPC
 {
+    public string Place;
+    public Dialog_Tree;
+    public bool Visibility;
+    public string Warningsigns = new warningsigns;
+    public string Name;
+    public uint Age;
+    public char Gender;
+
+    public bool SuisideRisk(warningssigns)
+    {
+        if warningsigns == True
+            ConsoleWriteLine("This Person does have warningsigns and should seek help")
+        else 
+            ConsoleWriteLine("This person does not show any warningsigsn")        
+    }
+    public void StartConversation
     
 }
