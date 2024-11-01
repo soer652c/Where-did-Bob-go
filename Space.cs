972915e94c2fa6e9358e03c26d7931ddee8f7a4c
Ayaan Hussain <ayhus24@student.sdu.dk>
2024-11-01 08:41:05 +0100

    Space update

--- Space.cs
+++ Space.cs
@@ -21,4 +21,29 @@ class Space : Node {
   public override Space FollowEdge (string direction) {
     return (Space) (base.FollowEdge(direction));
   }
+  
+  List<NPC> NPCList = new List<NPC>();
+  
+  //+ Movement_in_room():
+  public void Movement_in_room()
+  {
+    
+  }
+  //+ Move_to_NPC(NPC):
+  public override string ToString()
+  {
+    return SuisideRisk; 
+  }
+  public override bool Equals(object NPCList_ToCompare)
+  {
+    if (((NPC)NPCList_ToCompare).NPCList() == NPCList)
+    {
+      return true;
+      Console.WriteLine(SuisideRisk) 
+    }
+
+    return false;
+  }
+  //+ Display_Rooms():
+  //+ Display_NPC's():
 }
