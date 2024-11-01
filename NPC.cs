eb27edb390639faf11ab96e727fe33512720b57b
Søren Bomberg <soer652c@gmail.com>
2024-11-01 08:22:17 +0100

    Revert "NPC test"
    This reverts commit 13378ea76f05b3527a17c963ae8ac86094fd9ddd.

--- NPC.cs
+++ NPC.cs
@@ -1,93 +1,84 @@
-namespace DefaultNamespace
+namespace DefaultNamespace;
+
+public class NPC
 {
-    public class NPC
+    public string Place;
+    public Dialog_Tree;
+    public bool Visibility;
+    public bool Warningsigns;
+    public string Name;
+    public uint Age;
+    public char Gender;
+
+    public bool SuicideRisk(bool warningSigns)
     {
-        public string Place { get; set; }
-        public string DialogTree { get; set; }
-        public bool Visibility { get; set; }
-        public bool WarningSigns { get; set; }
-        public string Name { get; set; }
-        public uint Age { get; set; }
-        public char Gender { get; set; }
-
-        public bool SuicideRisk(bool warningSigns)
+        // Reads the imput from console
+        Console.ReadLine();
+        if (warningSigns)
         {
-            Console.WriteLine("Enter input:");
-            Console.ReadLine();
-            if (warningSigns)
-            {
-                Console.WriteLine("This person does have warning signs and should seek help.");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("This person does not show any warning signs.");
-                return false;
-            }
+            Console.WriteLine("This person does have warning signs and should seek help.");
+            return true; 
+            // Return true if there are warning signs
         }
-
-        public void StartConversation()
+        else 
         {
-            Console.WriteLine("Starting conversation");
-            Console.WriteLine("1. Greetings friend, How are you doing today?");
-            Console.WriteLine("2. Sup nerd hows it hangin?");
-            Console.WriteLine("3. Nevermind, I got somewhere else to be!");
-
-            string choice = Console.ReadLine();
-
-            switch (choice)
-            {
-                case "1":
-                    Console.WriteLine("I am doing very well thank you, What are you doing today?");
-                    break;
-                case "2":
-                    Console.WriteLine("I'm Skibidi Rizz Fam, What's the sitch?");
-                    break;
-                case "3":
-                    Console.WriteLine("Alrighty then, have a good day then");
-                    break;
-                default:
-                    Console.WriteLine("I don't understand that....");
-                    break;
-            }
+            Console.WriteLine("This person does not show any warning signs.");
+            return false; 
+            // Return false if there are no warning signs
         }
+    public void StartConversation()
+    {
+        // Short term code, to test if our conversation works in the first place, Gonna be revised later on
+        Console.WriteLine("Starting conversation");
+        Console.WriteLine("1. Greetings friend, How are you doing today?");
+        Console.WriteLine("2. Sup nerd hows it hangin?");
+        Console.WriteLine("3. Nevermind, i got somewhere else to be!");
 
-        public void StopConversation()
+        string choice = Console.ReadLine();
+
+        switch (choice)
         {
-            // Assuming `choice` is a class-level variable or passed as a parameter
-            // if (choice == "3")
-            // {
-            //     return Place;
-            // }
+            case "1":
+                Console.WriteLine("I am doign very well thank you, What are you doing today?.");
+                break;
+            case "2":
+                Console.WriteLine("Im Skibidi Rizz Fam, Whats the sitch?");
+                break;
+            case "3":
+                Console.WriteLine("Allrighty then, have a good day then");
+                break;
+            default:
+                Console.WriteLine("I don't understand that....");
+                break;
         }
+    }
+    public void StopConversation()
+    {
+        if choice == 3
+            return Place
+    }
 
-        public void UseMonocle(object monocle)
-        {
-            // Implementation for using a monocle
-        }
+    public void UseMonocle(Monocle)
+      {
 
-        public void Identifiers()
-        {
-            // Implementation for identifiers
-        }
+      }
 
-        public bool GuessInput(bool warningSigns)
-        {
-            Console.WriteLine("Enter your guess:");
-            string guess = Console.ReadLine();
-            return bool.TryParse(guess, out bool result) && result == warningSigns;
-        }
+    public void Identifiers()
+    {
+        string
+    }
+    public bool GuessImput(Warningsigns)
+    {
+        Guess = Console.ReadLine();
+    }
 
-        public void GuessCheck(bool guess)
-        {
-            if (guess)
-            {
-                Console.WriteLine("You are correct, now have a cookie!");
-            }
-            else
-            {
-                Console.WriteLine("Sorry, this person is fine and healthy");
-            }
-        }
+    public bool GuessCheck(Guess)
+    {
+        If Guess == true
+            Console.WriteLine("You are correct, now have a cookie!")
+
+        else Guess == false
+            Console.WriteLine("Sorry, this person is fine and healthy") 
+            
     }
 }
