e961aec64b0b8d565afaa157b91720efb865392d
KirstenGaarde <kigaa19@student.sdu.dk>
2024-11-12 14:07:08 +0100

    ææ

---
+++ Where_did_Bob_Go_VA/Command/CommandUse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Where_did_Bob_Go_VA.Command_NS;
+using Where_did_Bob_Go_VA.Game_NS;
+
+namespace Where_did_Bob_Go_VA.Command
+public class CommandUse : BaseCommand, ICommand
+{
+    public void Command_use()
+    {
+
+    }
+    public void Execute(Context context, string command, string[] parameters)
+    {
+        context.MakeDone();
+    }
+}
