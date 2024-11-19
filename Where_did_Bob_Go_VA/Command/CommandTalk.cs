b527486f4924fc81ccc05b88476497668cb7e23c
Rothausen <rasmusrothausen@gmail.com>
2024-11-19 12:25:15 +0100

    Comments

--- Where_did_Bob_Go_VA/Command/CommandTalk.cs
+++ Where_did_Bob_Go_VA/Command/CommandTalk.cs
@@ -32,24 +32,24 @@ namespace Where_did_Bob_Go_VA.Command_NS
 
         public void Execute(Context context, string command, string[] parameters)
         {
-            // .  
+            // . Grapping the current location from the context data
             Space current_location = Game.context.GetCurrent();
 
-            // .  
+            // .  Set the npc name to the first parameter
             string npcName = parameters[0];
 
-            // .  
+            // .  Making a if statement to check if the NPCMap is not empty and if the NPC is visible
             if ( (current_location.NPCMap.Count != 0) && (current_location.NPCMap[npcName].NPCvisibility) )
             {
-                // .  
+                // .  Set the current npc to the npc in the NPCMap
                 current_NPC = current_location.NPCMap[npcName];
-                
-                // .  
+
+                // .  Use the method Move_to_NPC to initiate the interaction with the NPC
                 current_location.Move_to_NPC(npcName);
             }
             else
             {
-                // .  
+                // .  Print that the there is no npc in the curren location with said name
                 Update_TextBox_Main("There is no one named " + npcName + " here.");
             }
          
