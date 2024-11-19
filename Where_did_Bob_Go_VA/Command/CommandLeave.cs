b4766c2f8c357d1795b122207c4dfcb93b78b1d2
Macch1 <awm18@live.dk>
2024-11-19 10:06:01 +0100

    Adjusting gamme and space for Command Talk and Command Leave.

--- Where_did_Bob_Go_VA/Command/CommandLeave.cs
+++ Where_did_Bob_Go_VA/Command/CommandLeave.cs
@@ -10,18 +10,20 @@ using Where_did_Bob_Go_VA.World_NS;
 using Where_did_Bob_Go_VA.Game_NS;
 
 using static Where_did_Bob_Go_VA.GUI_NS.GUI;
-using Microsoft.Win32;
+using static Where_did_Bob_Go_VA.Game_NS.Game;
 
 namespace Where_did_Bob_Go_VA.Command_NS
-
-public class CommandLeave : BaseCommand, ICommand
 {
-    public void Execute(Context context, string command, string[] parameters)
+
+    public class CommandLeave : BaseCommand, ICommand
     {
-        // registry. får vi fra vores Game.cs fil
-        // GetCommand trækker commandedn leave
-        // placeholder trækker fra CommandTalk, som viser hvilken NPC man snakker med
-        // så kører den StopConversation() for at afslutte samtalen
-        (registry.GetCommand("leave")).PlaceHoldder.StopConversation();
+        public void Execute(Context context, string command, string[] parameters)
+        {
+            // registry. får vi fra vores Game.cs fil
+            // GetCommand trækker commandedn leave
+            // placeholder trækker fra CommandTalk, som viser hvilken NPC man snakker med
+            // så kører den StopConversation() for at afslutte samtalen
+            (Game.registry.GetCommand("leave"))..StopConversation();
+        }
     }
 }
