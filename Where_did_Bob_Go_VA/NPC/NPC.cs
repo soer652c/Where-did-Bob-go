09c3fc5821911a1f2ab5b4c487b9dbfe8e8c4760
KirstenGaarde <kigaa19@student.sdu.dk>
2024-11-27 12:04:38 +0100

    StartConv. og Introduktion

--- Where_did_Bob_Go_VA/NPC/NPC.cs
+++ Where_did_Bob_Go_VA/NPC/NPC.cs
@@ -12,6 +12,7 @@ using Where_did_Bob_Go_VA.Game_NS;
 using static Where_did_Bob_Go_VA.GUI_NS.GUI;
 using System.Threading.Tasks;
 using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.Design;
 
 namespace Where_did_Bob_Go_VA.NPC_NS
 {
@@ -30,6 +31,8 @@ namespace Where_did_Bob_Go_VA.NPC_NS
         public string CurrentNPC;
         public string NPCriskLevel;
         public bool StartStop;
+
+
 
         private Dictionary<string, NPC_Dialog_Options> DialogOptionsTree;
 
@@ -61,12 +64,14 @@ namespace Where_did_Bob_Go_VA.NPC_NS
         //        // Return false if there are no warning signs
         //    }
         //}
-      
+
         public void StartConversation()
         {
-           StartStop = true;
+            StartStop = true;
             NPC_Dialog_Options initial_conversation = DialogOptionsTree["D00"];
             string next_conversation = "D00";
+            string ConversationOpstionreturn = "";
+            string[] GoBack ;
             while (StartStop == true && NPCvisibility == true)
             {
                 if (next_conversation == "D00")
@@ -76,101 +81,272 @@ namespace Where_did_Bob_Go_VA.NPC_NS
                 }
                 else if ((next_conversation == "D00.01") || (next_conversation == "DOO.02") || (next_conversation == "D00.03"))
                 {
-                    next_conversation = next_conversation + Conversation_Options2(DialogOptionsTree[next_conversation]);
+                    ConversationOpstionreturn = Conversation_Options2(DialogOptionsTree[next_conversation]);
+                    // inde i elseIf statamteten skal det som converstion2 returner gemmes i "Stringen" tjekke at det der returneres skal lægges til eller trækkes fra
+                    if ( ConversationOpstionreturn == "back")
+                    {
+                        GoBack = next_conversation.Split(".");
+                        next_conversation = "";
+                        int i = 0;
+                        for (; i < (GoBack.Length - 1); i++) 
+                        {
+                            if (i == 0)
+                            {
+                                next_conversation = GoBack[0];
+                            }
+                            else
+                            {
+                                next_conversation = next_conversation + "." + GoBack[i];
+                            }
+                        }
+                    }
+                    else 
+                    {
+                        next_conversation = next_conversation + Conversation_Options2(DialogOptionsTree[next_conversation]);
+                    }
+                    
                 }
                 else if ((next_conversation == "D00.01.01") || (next_conversation == "DOO.01.02") || (next_conversation == "D00.02.01") || (next_conversation == "D00.02.02") || (next_conversation == "DOO.03.01") || (next_conversation == "D00.03.02"))
                 {
-                    next_conversation = next_conversation + Conversation_Options0(DialogOptionsTree[next_conversation]);
+                    ConversationOpstionreturn = Conversation_Options0(DialogOptionsTree[next_conversation]);
+                    // inde i elseIf statamteten skal det som converstion2 returner gemmes i "Stringen" tjekke at det der returneres skal lægges til eller trækkes fra
+                    if (ConversationOpstionreturn == "back")
+                    {
+                        GoBack = next_conversation.Split(".");
+                        next_conversation = "";
+                        int i = 0;
+                        for (; i < (GoBack.Length - 1); i++)
+                        {
+                            if (i == 0)
+                            {
+                                next_conversation = GoBack[0];
+                            }
+                            else
+                            {
+                                next_conversation = next_conversation + "." + GoBack[i];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        next_conversation = next_conversation + Conversation_Options0(DialogOptionsTree[next_conversation]);
+                    }
 
                 }
-                  // Nested if, som har en valgmulighed i nr. 4. som modificere at vi kan gå tilbage i dialogtræet
-
             }
-
-            // Boolean StartConversaion (true)
-            // While loop, der tjekker om NPCvisibulity og er StartConversion sat til true
-            // While loop kalder en metoden Converstaion
-            // når stop conversation flipper til false. så skal startConveration lukke while loop
-
         }
 
-            // metode string Conversation (NPC_Dialog_Options)
-            private string Conversation_Options3(NPC_Dialog_Options dialog1)
+
+
+        private string Conversation_Options0(NPC_Dialog_Options dialog0)
+        {
+            Change_TextBox_Main(dialog0.text);
+            string[] backbox = { "Go back to previous conversation" };
+            string[] temper = new string[backbox.Length +2];
+
+            temper[0] = "Type 4 to pick option back: ";
+            temper[1] = "";
+
+            int k = 2;
+
+            for (int i = 0; i < backbox.Length; i++)
             {
-                Change_TextBox_Main(dialog1.text);
+                temper[k] = backbox[i];
 
-                string[] temper = new string[dialog1.Options_1.Length + dialog1.Options_2.Length + dialog1.Options_3.Length + 1];
+                k++;
 
-                int k = 0;
-                for (int i = 0; i < dialog1.Options_1.Length; i++)
-                {
-                            temper[k] = dialog1.Options_1[i];
-                    k++;
-                }
+            }
+            
+            Change_TextBox_Options(temper);
+            Update_GUI();
 
-                for (int i = 0; i < dialog1.Options_2.Length; i++)
-                    {
-                    temper[k] = dialog1.Options_2[i];
-                    k++;
-                }
+            string answer = Console.ReadLine();
 
-                for (int i = 0; i < dialog1.Options_3.Length; i++)
-                {
-                    temper[k] = dialog1.Options_3[i];
-                    k++;
-                }
-
-                temper[k] = "Retrun to previous";
-
-                Change_TextBox_Options(temper);
-                Update_GUI();
-
-                string answer= Console.ReadLine();
-
-                switch (answer)
-                {
-                    case "1":
-                        // Handle option 1
-                        return ".01";
-                        break;
-
-                    case "2":
-                        // Handle option 2
-                        return ".02";
-                        break;
-
-                    case "3":
-                        // Handle option 3
-                        return ".03";
-                        break;
-
-                    case "4":
+            switch (answer)
+            {
+                case "4":
+                    return "back";
                     //Go back
                     break;
+                case "leave":
+                    Game.registry.GetCommand("leave");
+                    break;
 
-                    default:
-                        Console.WriteLine("There no options here containg this")
-                            // Handle invalid input
-                        break;
-                }
+                default:
+                    Console.WriteLine("There no options here containg this");
+                    // Handle invalid input
+                    break;
+            }
+            return "";
+        }
+
+
+        // Boolean StartConversaion (true)
+        // While loop, der tjekker om NPCvisibulity og er StartConversion sat til true
+        // While loop kalder en metoden Converstaion
+        // når stop conversation flipper til false. så skal startConveration lukke while loop
+        private string Conversation_Options2(NPC_Dialog_Options dialog2)
+        {
+            Change_TextBox_Main(dialog2.text);
+            string[] backbox = { "Go back to previous conversation" };
+            string[] temper = new string[dialog2.Options_1.Length + dialog2.Options_2.Length + backbox.Length +2];
+
+            temper[0] = "Type 1, 2 or 4, to pick options: ";
+            temper[1] = "";
+
+            int k = 2;
+
+            for (int i = 0; i < dialog2.Options_1.Length; i++)
+            {
+                temper[k] = dialog2.Options_1[i];
+
+                k++;
             }
 
-                                // hvis du vælger 1, skal den hente teksten fra 1 i dialogtræret. Ved hjælp af Chase/break;
-                                // returner tilbage til StartConversation. Der giver nye parameter, der gør det muligt at navigere i dialogtræet
+            for (int i = 0; i < dialog2.Options_2.Length; i++)
+            {
+                temper[k] = dialog2.Options_2[i];
 
-                                //metode string Conversation (NPC_Dialog_OPtions2)
+                k++;
 
-                                //metode string Conversation (NPC_Dialog_Options0)
+            }
+
+            for (int i = 0; i < backbox.Length; i++)
+            {
+                temper[k] = backbox[i];
+
+                k++;
+
+            }
+
+            Change_TextBox_Options(temper);
+            Update_GUI();
+
+            string answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "1":
+                    // Handle option 1
+                    return ".01";
+                    break;
+
+                case "2":
+                    // Handle option 2
+                    return ".02";
+                    break;
+
+                case "4":
+                    return "back";
+                    //Go back
+                    break;
+                case "leave":
+                    Game.registry.GetCommand("leave");
+                    break;
+
+                default:
+                    Console.WriteLine("There no options containing " + answer);
+                    // Handle invalid input
+                    break;
+            }
+            return "";
+        }
+
+
+        // metode string Conversation (NPC_Dialog_Options)
+        private string Conversation_Options3(NPC_Dialog_Options dialog1)
+        {
+            Change_TextBox_Main(dialog1.text);
+            string[] backbox = { "Go back to previous conversation" };
+            string[] temper = new string[dialog1.Options_1.Length + dialog1.Options_2.Length + dialog1.Options_3.Length + backbox.Length +2];
+
+
+            temper[0] = "Type 1,2,3 or 4 to pick options: ";
+            temper[1] = "";
+
+            int k = 2;
+
+            for (int i = 0; i < dialog1.Options_1.Length; i++)
+            {
+                temper[k] = dialog1.Options_1[i];
+                k++;
+            }
+
+            for (int i = 0; i < dialog1.Options_2.Length; i++)
+            {
+                temper[k] = dialog1.Options_2[i];
+                k++;
+            }
+
+            for (int i = 0; i < dialog1.Options_3.Length; i++)
+            {
+                temper[k] = dialog1.Options_3[i];
+                k++;
+            }
+            for (int i = 0; i < backbox.Length; i++)
+            {
+                temper[k] = backbox[i];
+
+                k++;
+
+            }
+
+            Change_TextBox_Options(temper);
+            Update_GUI();
+
+            string answer= Console.ReadLine();
+
+            switch (answer)
+            {
+                case "1":
+                    // Handle option 1
+                    return ".01";
+                    break;
+
+                case "2":
+                    // Handle option 2
+                    return ".02";
+                    break;
+
+                case "3":
+                    // Handle option 3
+                    return ".03";
+                break;
+
+                case "4":
+                return // "back"
+                //Go back
+                break;
+
+                case "leave":
+                Game.registry.GetCommand("leave");
+                break;
+
+            default:
+                Console.WriteLine("There no options here containg this");
+                        // Handle invalid input
+                    break;
+            }
+        return "";
+        }
+
+                                
+    // hvis du vælger 1, skal den hente teksten fra 1 i dialogtræret. Ved hjælp af Chase/break;
+// returner tilbage til StartConversation. Der giver nye parameter, der gør det muligt at navigere i dialogtræet
+
+//metode string Conversation (NPC_Dialog_OPtions2)
+
+//metode string Conversation (NPC_Dialog_Options0)
        public void StopConversation()
        {
-                    // If statemat flipper boolean StartConversiation(false)
+            StartStop = false;
        }
                     
 
-                    //public void UseMonocle(Moncle monocle)
-                    //{
+    //public void UseMonocle(Moncle monocle)
+    //{
 
-                    //}
+    //}
 
         private void NPC_identifiers(string NPCName, int NPCage, string NPCgender)
         {
