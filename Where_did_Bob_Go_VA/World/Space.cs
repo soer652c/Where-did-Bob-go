0299888f4184d55266a7afbac890600798b5f115
Rothausen <rasmusrothausen@gmail.com>
2024-11-28 12:45:34 +0100

    Food and NPC placements

--- Where_did_Bob_Go_VA/World/Space.cs
+++ Where_did_Bob_Go_VA/World/Space.cs
@@ -31,7 +31,11 @@ namespace Where_did_Bob_Go_VA.World_NS
 
 
 
-
+        public Space(String name, string description) : base(name)
+        {
+            this.description = description;
+            this.name = name;
+        }
         public Space(String name, string description, Dictionary<string, NPC> Init_NPCMap) : base(name)
         {
             this.description = description;
@@ -57,6 +61,14 @@ namespace Where_did_Bob_Go_VA.World_NS
             this.name = name;
             this.NPC_Map.Add(Init_NPC.NPC_Name, Init_NPC);
             this.NPC_Map.Add(Init_NPC2.NPC_Name, Init_NPC2);
+        }
+        public Space(String name, string description, NPC Init_NPC, NPC Init_NPC2, Item Init_Item) : base(name)
+        {
+            this.description = description;
+            this.name = name;
+            this.NPC_Map.Add(Init_NPC.NPC_Name, Init_NPC);
+            this.NPC_Map.Add(Init_NPC2.NPC_Name, Init_NPC2);
+            this.Item_Map.Add(Init_Item.Name, Init_Item);
         }
 
         public Space(String name, string description, Dictionary<string, NPC> Init_NPCMap, Dictionary<string, Item> Init_ItemMap) : base(name)
