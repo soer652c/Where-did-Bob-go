ec5de476b7ac636fe48590e4288cc1028d0a99f0
KirstenGaarde <kigaa19@student.sdu.dk>
2024-12-03 12:38:47 +0100

    Nu med person/people

--- Where_did_Bob_Go_VA/Game.cs
+++ Where_did_Bob_Go_VA/Game.cs
@@ -98,17 +98,17 @@ namespace Where_did_Bob_Go_VA
             Change_TextBox_Options("Press Enter...");
             Update_GUI();
             Console.ReadLine(); // når man trykker enter, så går den videre til næste del af teksten
-            Update_TextBox_Main("The Story of the game \n You’ve just arrived to the town shrouded in secrets.\n You’ll meet 12 unique NPCs \n each with their own stories and challenges.\n Some may hold clues to what the NPC's are struggling with. \n It’s up to you to connect with them \n ask the right questions \n and decide how best to help");
+            Update_TextBox_Main("The Story of the game \n You’ve just arrived to the town shrouded in secrets.\n You’ll meet 12 unique peoples \n each with their own stories and challenges.\n Some may hold clues to what the person is struggling with. \n It’s up to you to connect with them \n ask the right questions \n and decide how best to help");
             Console.ReadLine();
             Update_TextBox_Main("But be warned: not everyone needs saving, and not everyone can be saved");
             Console.ReadLine();
-            Update_TextBox_Main("How to Play \n Where did Bob go is a text-based terminal game.\n You’ll explore the town \n and interact with NPCs entirely through text commands. \n Your choices will shape the story \n as you navigate through dialogues. \n    When you have meet an NPC and talked to them. \n   You have to guess their level of suisiderisk. \n    you do so by typing guess name of NPC and their level. \n Their level can either be High, Moderate or None. \n   If you guessed the rigth level the NPC will become invisubal");
+            Update_TextBox_Main("How to Play \n Where did Bob go is a text-based terminal game.\n You’ll explore the town \n and interact with the person entirely through text commands. \n Your choices will shape the story \n as you navigate through dialogues. \n    When you have meet an person and talked to them. \n   You have to guess their level of suisiderisk. \n    you do so by typing guess name of person and their level. \n Their level can either be High, Moderate or None. \n   If you guessed the rigth level the person will become invisubal");
             Console.ReadLine();
             Update_TextBox_Main("Here's how it works:\n 1.Navigate the World: \n Use simple text commands like 'go outside' \n to look around \n to move through the game’s location \n Explore key areas like the school or park \n each holding unique characters and clues. \n    There will be several food-items shown on differnt locations. \n    These items can give you an extra life \n    type use and then the item"); 
             Console.ReadLine();
             Update_TextBox_Main("Engage in Dialogue:\n When you meet an NPC \n initiate a conversation with commands like “talk” or “leave”. \n Choose the responses by typing 1, 2, or 3 \n this steer the conversation."); 
             Console.ReadLine();
-            Update_TextBox_Main("Gather Clues:\n Pay close attention to the NPCs' words and behavior. \n Subtle hints could reveal their emotional state.\n Make Critical Choices:\n Each decision affects the story.Will you dig deeper, or let things slide ? \n Some paths may close forever depending on your responses.");
+            Update_TextBox_Main("Gather Clues:\n Pay close attention to the people words and behavior. \n Subtle hints could reveal their emotional state.\n Make Critical Choices:\n Each decision affects the story.Will you dig deeper, or let things slide ? \n Some paths may close forever depending on your responses.");
             Console.ReadLine();
             Update_TextBox_Main("Why It Matters \n the towns inhabitants have lives as complex as the world around them. \n By choosing the right words and actions \n you could make a real difference \n or miss the chance entirely. \n Will you be the one to help those who need it the most? \n Or will the weight of your choices leave unanswered questions behind?");
             Console.ReadLine();
